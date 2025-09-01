cfb4410092e84b5fa588e626ea5388ab5e287d57
Marichandran73 <royalmari7339@gmail.com>
2025-09-01 11:32:27 +0530

    fix: i changed file like privateRoutes to PrivateRoutes

--- src/pages/utils/privateRoutes.jsx
+++ src/pages/utils/privateRoutes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAuth} from '../../context/authContext';
 import { Navigate } from "react-router-dom";
 
-const privateRoutes = ({children}) => {
+const PrivateRoutes = ({children}) => {
     const {user, loading}=useAuth();
 
       if (loading) {
@@ -21,4 +21,4 @@ const privateRoutes = ({children}) => {
   )
 }
 
-export default privateRoutes
+export default PrivateRoutes
